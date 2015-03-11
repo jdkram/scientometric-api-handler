2b2002728814a0c384c02f89a967ac1afea5db86
Jonathon Kram <jdkram@gmail.com>
2015-03-11 13:10:11 +0000

    Add ability to run from command line using 'sah' command

---
+++ sample_config.rb
@@ -0,0 +1,9 @@
+# API keys
+
+# http://api.altmetric.com/
+ALTMETRIC_API_KEY = '' 
+
+# Pushover: notifications to mobile: https://pushover.net/
+# API: https://pushover.net/api
+PUSHOVER_API_KEY = ''
+PUSHOVER_USER_KEY = ''
