55f7b97286edca231bb4201ce2d5ecbf00bd7061
Jonathon Kram <jdkram@gmail.com>
2015-07-21 10:03:14 +0100

    Update temp task file

--- bin/task.rb
+++ bin/task.rb
@@ -1,14 +1,14 @@
 Dir["../lib/*.rb"].each {|file| require_relative file }
 
 input = '../input/PubMed_1000_original_2014-06-15.csv'
-output = '../output/PubMed_1000_original_2014-06-15_OUTPUT.csv'
-api = 'epmc'
+output = '../output/PubMed_1000_original_altmetric.csv'
+api = 'altmetric'
 
 # input = '../input/MOPs_Jo_M_June_2015.csv'
 # output = '../output/MOPs_Jo_M_June_2015.csv'
 # api = 'epmc'
 
-csv_create(input, output_csv: output, api: api)
+csv_create(input, api: api)
 # csv_create(input, output_csv: output, api: api)
 # get_altmetric('24668137', false)
 
