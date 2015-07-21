36e46dede1f2a49439ddd8477ba1174ef5190890
Jonathon Kram <jdkram@gmail.com>
2015-07-21 18:36:00 +0100

    Tests work with generalised ID classifier, not just pmid or DOI checker

--- tests/test_check_classify_id.rb
+++ tests/test_check_classify_id.rb
@@ -8,8 +8,8 @@ class TestPMIDorDOI < Minitest::Unit::TestCase
   SAMPLE_DOI = 'DOI/a.DOI.here'
 
   def test_identify_a_pmid
-    assert_equal 'pmid', pmid_or_doi(SAMPLE_PMID)
-    assert_equal 'doi', pmid_or_doi(SAMPLE_DOI)
+    assert_equal 'pmid', classify_id(SAMPLE_PMID)
+    assert_equal 'doi', classify_id(SAMPLE_DOI)
   end
 
 end
