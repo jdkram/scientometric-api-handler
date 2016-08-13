c5a4ff806b2ed01ef876403f9414ab4f4c5ce297
Jonathon Kram <jdkram@gmail.com>
2016-08-13 13:06:43 +0100

    Replace iCite web scraper with API wrapper

--- lib/apis/icite.rb
+++ lib/apis/icite.rb
@@ -1,38 +1,31 @@
 require 'require_all'
-require 'mechanize'
+require 'open-uri'
 require 'csv'
+require 'json'
+require 'colorize'
 
-ICITE_URL = "https://icite.od.nih.gov/analysis"
+ICITE_URL = "https://icite.od.nih.gov/api/pubs?pmids="
+ICITE_FIELDS = ["pmid", "authors", "citation_count", "citations_per_year", "expected citations_per_year", "field_citation_rate", "is_research_article", "journal", "nih_percentile", "relative_citation_ratio", "title", "year"]
 
 def get_rcrs(pmids)
-  agent = Mechanize.new
-  agent.pluggable_parser.default = Mechanize::Download
-  page = agent.get(ICITE_URL)
-  form = page.forms.first
-  form.fields.first.value = pmids
-  # pp page
-  page = agent.submit(form, form.buttons.first)
-  tr = page.parser.xpath("//tr[contains(@class, 'pub-data')]")
-  articles = []
-  tr.each do |tr|
-    article = {}
-    article[:pmid] = tr.attr('data-pmid')
-    article[:cited_per_year] = tr.attr('data-cited-per-year')
-    article[:rcr] = tr.attr('data-rcr')
-    articles << article
-  end
+  url = ICITE_URL + pmids
+  icite_response = open(url)
+  icite_json = JSON.parse(icite_response.read)
+  articles = icite_json["data"]
   return articles
 end
 
 def batch_process_pmids_in_icite(input_csv:, output_csv:)
   pmids = CSV.read(input_csv, {headers: true})
   pmids = pmids.to_a.flatten
-  CSV.open(output_csv, "a") { |csv| csv << ['pmid', 'cited_per_year', 'RCR'] }
+  pmids.shift if pmids.first =~ /pmid/ # Remove first row if it's a header called 'pmid'
+  CSV.open(output_csv, "a") { |csv| csv << ICITE_FIELDS }
   num_blocks = (pmids.length / 200) # block 0 counts, no need to ceil
   puts "Beginning #{num_blocks} queries..."
   (0..num_blocks).each do |i|
     begin
-    str = pmids[i*200...(i+1)*200].join(' ') # e.g. 0 to 199, 200 to 399
+    tries ||= 5
+    str = pmids[i*200...(i+1)*200].join(',') # e.g. 0 to 199, 200 to 399
     articles = get_rcrs(str)
     CSV.open(output_csv, "a") do |csv|  
       articles.each do |article|
@@ -41,10 +34,19 @@ def batch_process_pmids_in_icite(input_csv:, output_csv:)
     end
       puts "Added #{articles.length} articles - block #{i}/#{num_blocks}".colorize(:green)
     sleep 1
-    rescue Mechanize::ResponseCodeError
-      puts "Error asking for batch beginning #{pmids[i*200].to_s} - block #{i}/#{num_blocks}"
-      sleep 1
-    next
+    rescue OpenURI::HTTPError => e
+        if e.message == '502 Proxy Error'
+          puts "Connection failed, retrying...".colorize(:yellow)
+          retry unless (tries -= 1).zero? # 5 tries per request
+        end
+      rescue Errno::ENETUNREACH => e
+        if (tries -= 1).zero?
+          puts "Connection failed, retrying...".colorize(:yellow)
+          retry
+        else
+          puts "5 consecutive errors connecting: #{e.message}"
+        end
     end
   end
 end
+
